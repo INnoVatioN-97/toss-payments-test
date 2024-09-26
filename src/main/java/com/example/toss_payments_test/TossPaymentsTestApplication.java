264a8a950eceb8cd97319d0efd9aea8cb4b5c473
INnoVatioN-97 <koyoungil97@gmail.com>
2024-09-26 14:52:26 +0900

    feat: 신규 프로젝트 생성

---
+++ src/main/java/com/example/toss_payments_test/TossPaymentsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.toss_payments_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossPaymentsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TossPaymentsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toss_payments_test/tosspaymentstest/TossPaymentsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toss_payments_test.tosspaymentstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossPaymentsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
